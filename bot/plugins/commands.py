06aa1c0b23bf815a07779a26649acacd01907b18
Shabee369 <88023919+Shabee369@users.noreply.github.com>
2021-07-27 12:44:44 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -56,7 +56,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Join Channrl💕', url="https://t.me/moviess_hubb"
                                 )
                         ]
                     ]
